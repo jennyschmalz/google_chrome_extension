e03ca210321339c3f2e3c924e6c30a8d0f256a15
Jenny Schmalz <jschmalzperez@gmail.com>
2023-01-20 10:06:14 -0800

    add styles and timer js

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
 console.log("This is a popup!");
+
+document.addEventListener('DOMContentLoaded', (e) => {
+  let input = window.prompt('Please enter time in minutes for timer:');
+  input = parseInt(input);
+
+  while (input <= 0 || !input) {
+    input = window.prompt('Please enter time in minutes for timer:');
+  }
+
+  console.log(input);
+
+  const startingMinutes = input;
+  let time = startingMinutes * 60;
+
+  const countdown = document.getElementById('countdown');
+
+  const updateTimer = () => {
+    const minutes = Math.floor(time / 60);
+    let seconds = time % 60;
+
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+
+    countdown.innerHTML = `${minutes} : ${seconds}`;
+    time--;
+  }
+
+  setInterval(updateTimer, 1000);
+})
+
